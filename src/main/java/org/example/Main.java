56b81e0f0db1ac628eeeda55d9a16e7b9f27352b
Trương Nguyễn Hương Giang <143255970+GiangNguyen0208@users.noreply.github.com>
2025-07-26 18:29:11 +0700

    Using textures

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,7 +17,6 @@ public class Main {
 
         // Create a window
         long window = GLFW.glfwCreateWindow(800, 600, "Window", 0, 0);
-
         if (window == 0) {
             GLFW.glfwTerminate();
             throw new RuntimeException("Failed to create the GLFW window");
@@ -33,65 +32,41 @@ public class Main {
         // Set clear color to black
         GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-//        float x = 0;
-        float color_red = 1;
-        float color_blue= 0;
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+
+        Texture tex = new Texture("D:\\Code\\SubjectProject\\DHMT\\ComputerGraphics\\src\\main\\resources\\img\\mushroom_sprite_pack.png");
 
         // Main loop
         while (!GLFW.glfwWindowShouldClose(window)) {
             // Esc button to close window
             if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_TRUE) {
-                glfwDestroyWindow(window);
+                glfwSetWindowShouldClose(window, true);
             }
 
             // Poll for window events
             GLFW.glfwPollEvents();
 
-            // Execute Action Change by Key A on Board
-//            if (glfwGetKey(window, GLFW_KEY_A) == GLFW_TRUE) {
-////                x += 0.001f;
-//                color_red = 0.25f;
-//                color_blue = 1;
-//            } else {
-//                color_blue = 0.25f;
-//                color_red = 1;
-//            }
-
-            // Execute Action Change by Mouse Click
-//            if (glfwGetMouseButton(window, 0) == GLFW_TRUE) {
-//                color_red = 0.25f;
-//                color_blue = 1;
-//            }
-
             // Clear the screen
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
-
+            // Bind texture
+            tex.bind();
 
             // Draw Quads
             glBegin(GL11.GL_QUADS);
-                glColor4f(color_red,0,color_blue,0);
-                glVertex2f(-0.5f, 0.5f);
-
-//                glColor4f(0,1,0,0);
-                glVertex2f(0.5f, 0.5f);
-
-//                glColor4f(0,0,1,0);
-                glVertex2f(0.5f, -0.5f);
-
-//                glColor4f(0,0,0,1);
-                glVertex2f(-0.5f, -0.5f);
+            // Toàn bộ sprite pack (0-1 cho cả 192x32)
+                glTexCoord2f(0.0f, 0.0f); glVertex2f(-0.5f, 0.5f);  // Top-left
+                glTexCoord2f(1.0f, 0.0f); glVertex2f(0.5f, 0.5f);   // Top-right
+                glTexCoord2f(1.0f, 1.0f); glVertex2f(0.5f, -0.5f); // Bottom-right
+                glTexCoord2f(0.0f, 1.0f); glVertex2f(-0.5f, -0.5f); // Bottom-left
             glEnd();
 
             // Swap buffers
             GLFW.glfwSwapBuffers(window);
-
-
-
-
         }
 
         // Clean up
+        tex.cleanup();
         GLFW.glfwDestroyWindow(window);
         GLFW.glfwTerminate();
     }
