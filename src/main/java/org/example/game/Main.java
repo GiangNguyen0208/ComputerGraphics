7febc64c80dea4164ca2b2858358f2be40423ed6
Trương Nguyễn Hương Giang <143255970+GiangNguyen0208@users.noreply.github.com>
2025-07-28 18:35:34 +0700

    Rendering Tiles

--- src/main/java/org/example/game/Main.java
+++ src/main/java/org/example/game/Main.java
@@ -6,6 +6,7 @@ import org.example.render.Camera;
 import org.example.render.Model;
 import org.example.render.Shader;
 import org.example.render.Texture;
+import org.example.world.TileRenderer;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.lwjgl.glfw.GLFW;
@@ -37,39 +38,39 @@ public class Main {
         GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
         Camera camera = new Camera(window.getWidth(), window.getHeight());
-
         GL11.glEnable(GL11.GL_TEXTURE_2D);
 
-        // Define vertices for a square (2 triangles)
-        float[] vertices = new float[] {
-                -0.5f, 0.5f, 0,   // TOP LEFT       0
-                0.5f, 0.5f, 0,    // TOP RIGHT      1
-                0.5f, -0.5f, 0,   // BOTTOM RIGHT   2
-                -0.5f, -0.5f, 0,  // BOTTOM LEFT    3
-        };
+        TileRenderer tiles = new TileRenderer();
 
-        // Define texture coordinates for 2 triangles
-        float[] texture = new float[] {
-                0.0f, 0.0f,  // TOP LEFT        0
-                1.0f, 0.0f,  // TOP RIGHT       1
-                1.0f, 1.0f,  // BOTTOM RIGHT    2
-                0.0f, 1.0f,  // BOTTOM LEFT     3
-        };
-
-        int[] indices = new int[] {
-                0, 1, 2,
-                2, 3, 0,
-        };
-
-        Model model = new Model(vertices, texture, indices);
+//        // Define vertices for a square (2 triangles)
+//        float[] vertices = new float[] {
+//                -0.5f, 0.5f, 0,   // TOP LEFT       0
+//                0.5f, 0.5f, 0,    // TOP RIGHT      1
+//                0.5f, -0.5f, 0,   // BOTTOM RIGHT   2
+//                -0.5f, -0.5f, 0,  // BOTTOM LEFT    3
+//        };
+//
+//        // Define texture coordinates for 2 triangles
+//        float[] texture = new float[] {
+//                0.0f, 0.0f,  // TOP LEFT        0
+//                1.0f, 0.0f,  // TOP RIGHT       1
+//                1.0f, 1.0f,  // BOTTOM RIGHT    2
+//                0.0f, 1.0f,  // BOTTOM LEFT     3
+//        };
+//
+//        int[] indices = new int[] {
+//                0, 1, 2,
+//                2, 3, 0,
+//        };
+//
+//        Model model = new Model(vertices, texture, indices);
         Shader shader = new Shader("shader");
 
-        Texture tex = new Texture("D:\\Code\\SubjectProject\\DHMT\\ComputerGraphics\\src\\main\\resources\\img\\screenshot_67.png");
-
+        Texture tex = new Texture("src/main/resources/img/screenshot_67.png");
 
         Matrix4f scale = new Matrix4f()
-                .translate(new Vector3f(100, 0, 0))
-                .scale(128);
+                .translate(new Vector3f(0, 0, 0))
+                .scale(32);
 
         Matrix4f target = new Matrix4f();
 
@@ -120,12 +121,17 @@ public class Main {
                 GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
                 // Bind texture
-                shader.bind();
-                shader.setUniform("sampler", 0);
-                shader.setUniform("projection", camera.getProjection().mul(target));
-                tex.bind(0);
-                model.render();
+//                shader.bind();
+//                shader.setUniform("sampler", 0);
+//                shader.setUniform("projection", camera.getProjection().mul(target));
+//                model.render();
+//                tex.bind(0);
 
+                for (int i = 0; i < 8; i++) {
+                    for (int j = 0; j < 4; j++) {
+                        tiles.renderTile((byte) 0, i, j, shader, scale, camera);
+                    }
+                }
                 window.swapBuffers();
                 frames++;
             }
---
+++ src/main/java/org/example/world/TileRenderer.java
@@ -0,0 +1,66 @@
+package org.example.world;
+
+import org.example.render.Camera;
+import org.example.render.Model;
+import org.example.render.Shader;
+import org.example.render.Texture;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+
+import java.util.HashMap;
+
+public class TileRenderer {
+    private HashMap<String, Texture> tile_textures;
+    private Model model;
+
+    public TileRenderer() {
+        tile_textures = new HashMap<String, Texture>();
+        // Define vertices for a square (2 triangles)
+        float[] vertices = new float[] {
+                -0.5f, 0.5f, 0,   // TOP LEFT       0
+                0.5f, 0.5f, 0,    // TOP RIGHT      1
+                0.5f, -0.5f, 0,   // BOTTOM RIGHT   2
+                -0.5f, -0.5f, 0,  // BOTTOM LEFT    3
+        };
+
+        // Define texture coordinates for 2 triangles
+        float[] texture = new float[] {
+                0.0f, 0.0f,  // TOP LEFT        0
+                1.0f, 0.0f,  // TOP RIGHT       1
+                1.0f, 1.0f,  // BOTTOM RIGHT    2
+                0.0f, 1.0f,  // BOTTOM LEFT     3
+        };
+
+        int[] indices = new int[] {
+                0, 1, 2,
+                2, 3, 0,
+        };
+
+        model = new Model(vertices, texture, indices);
+
+        for (int i = 0; i < Tile.tiles.length; i++) {
+            if (Tile.tiles[i] != null) {
+                if (!tile_textures.containsKey(Tile.tiles[i].getTexture())) {
+                    String tex = Tile.tiles[i].getTexture();
+                    tile_textures.put(tex, new Texture("src/main/resources/img/"+tex+".png"));
+                }
+            }
+        }
+    }
+    public void renderTile(byte id, int x, int y, Shader shader, Matrix4f world, Camera camera) {
+        shader.bind();
+        if (tile_textures.containsKey(Tile.tiles[id].getTexture())) {
+            tile_textures.get(Tile.tiles[id].getTexture()).bind(0);
+        }
+        Matrix4f tile_pos = new Matrix4f().translate(new Vector3f(x*2, y*2, 0));
+        Matrix4f target = new Matrix4f();
+
+        camera.getProjection().mul(world, target);
+        target.mul(tile_pos);
+
+        shader.setUniform("sampler", 0);
+        shader.setUniform("projection", target);
+
+        model.render();
+    }
+}
